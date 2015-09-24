15e6da006b7993e902882e47b22b10f2605fc466
arleigh-atkinson <arleigh.atkinson@gmail.com>
2015-09-24 15:55:34 -0600

    Devise working

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def match
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  
   devise_for :users
+
+  get 'publications/new'
+
+  get 'publications/edit'
+
+  root 'users#index'
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
